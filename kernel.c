b834cf268e2bb0a18014d2617ae969903d904c24
noizhardware <noisefuel@gmail.com>
2021-04-18 11:54:56 +0200

    nj

--- kernel.c
+++ kernel.c
@@ -1,7 +1,8 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-/* other libs available in freestanding mode: <float.h>, <iso646.h>, <limits.h>, and <stdarg.h>*/
+/*** TODO
+
+*/
+
+#define K0_VERSION "2021d18-1147"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -12,7 +13,18 @@
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
- 
+
+/*** DEFINES */
+/* DEFINES end. */
+
+/*** INCLUDES */
+     #include <stdbool.h>
+     #include <stddef.h>
+     #include <stdint.h>
+     /* other libs available in freestanding mode: <float.h>, <iso646.h>, <limits.h>, and <stdarg.h>*/
+/* INCLUDES end. */
+
+/*** TYPEDEFS */
 /* Hardware text mode color constants. */
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
@@ -32,35 +44,62 @@ enum vga_color {
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
 };
+/* TYPEDEFS end. */
+
+/*** GLOBALS */
+     static const size_t VGA_WIDTH = 80;
+     static const size_t VGA_HEIGHT = 25;
+      
+     size_t terminal_row;
+     size_t terminal_column;
+     uint8_t terminal_color;
+     uint16_t* terminal_buffer;
+/* GLOBALS end. */
+
+/*** FUNCTION DECLARATIONS */
+
+     static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg);
+     static inline uint16_t vga_entry(unsigned char uc, uint8_t color);
+
+     size_t strlen(const char* str);
+
+     void terminal_initialize(void);
+     void terminal_setcolor(uint8_t color);
+     void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+     void terminal_putchar(char c);
+     void terminal_write(const char* data, size_t size);
+     void terminal_writestring(const char* data);
+
+/* FUNCTION DECLARATIONS end. */
+
+/*** MAIN */
+void kernel_main(void){
+	/* Initialize terminal interface */
+	terminal_initialize();
  
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
-{
+	/* Newline support is left as an exercise. */
+	terminal_writestring("PORCODIO!\n");
+}
+/* MAIN end. */
+
+/*** FUNCTION DEFINITIONS */
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg){
 	return fg | bg << 4;
 }
  
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
-{
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color){
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
  
-size_t strlen(const char* str) 
-{
+size_t strlen(const char* str){
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
  
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
  
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
-{
+void terminal_initialize(void){
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
@@ -73,19 +112,16 @@ void terminal_initialize(void)
 	}
 }
  
-void terminal_setcolor(uint8_t color) 
-{
+void terminal_setcolor(uint8_t color){
 	terminal_color = color;
 }
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y){
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
  
-void terminal_putchar(char c) 
-{
+void terminal_putchar(char c){
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -94,22 +130,12 @@ void terminal_putchar(char c)
 	}
 }
  
-void terminal_write(const char* data, size_t size) 
-{
+void terminal_write(const char* data, size_t size){
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
  
-void terminal_writestring(const char* data) 
-{
+void terminal_writestring(const char* data){
 	terminal_write(data, strlen(data));
 }
- 
-void kernel_main(void) 
-{
-	/* Initialize terminal interface */
-	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
-	terminal_writestring("PORCODIO!\n");
-}
+/* FUNCTION DEFINITIONS end. */
